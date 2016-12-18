61558e14e41f10d6e24ba5bfbd1f4f4f260e266b
Maksim Iliukovich <maksim_iliukovich@epam.com>
2016-12-18 17:55:26 +0300

    404 error

--- src/main/java/test_system/controller/RestControllerAdvice.java
+++ src/main/java/test_system/controller/RestControllerAdvice.java
@@ -1,15 +1,24 @@
 package test_system.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import test_system.exception.NotFoundException;
 
 @SuppressWarnings("unused")
 @ControllerAdvice
 public class RestControllerAdvice {
 
     @ExceptionHandler(Exception.class)
-    final String handle(final Exception exception) {
+    final String handle(final Exception exception, final Model model) {
         exception.printStackTrace();
+
+        // todo: lets save template in class of exception by the custom annotations
+        if (exception instanceof NotFoundException) {
+            model.addAttribute("reason", exception.getMessage());
+            return Template.ERROR_404_TEMPLATE;
+        }
+
         return Template.ERROR_TEMPLATE;
     }
 }
--- src/main/java/test_system/controller/Template.java
+++ src/main/java/test_system/controller/Template.java
@@ -5,4 +5,6 @@ final class Template {
     static final String WORK_PAGE_TEMPLATE = "work";
     static final String THEORY_PAGE_TEMPLATE = "theory";
     static final String ERROR_TEMPLATE = "error";
+    static final String TEST_PAGE_TEMPLATE = "test";
+    static final String ERROR_404_TEMPLATE = "404";
 }
